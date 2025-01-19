f30ea233c200f32f6f56d198b2c2a7284ae9a623
Mine <mine-rala@hotmail.com>
2025-01-19 15:24:18 +0300

    Initial Commit

---
+++ AuthenticationView/AuthenticationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationViewApp.swift
+//  AuthenticationView
+//
+//  Created by Mine Rala on 19.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
